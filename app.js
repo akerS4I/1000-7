522e9338e0298c6da5c2711ec795db7819b01b48
akerS4I <108138366+akerS4I@users.noreply.github.com>
2022-09-23 17:54:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const result = document.querySelector("p");
+const body = document.querySelector("body");
+const btn = document.querySelector("btn");
+let i = 1000;
+
+
+function ghoul() {
+  while (i > 0){
+    console.log(`${i} - 7 = ${i-7}`)
+    i -= 7
+}}
